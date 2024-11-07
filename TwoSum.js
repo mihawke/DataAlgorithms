0fe6f98a0aee723ba6ffa18cddefd6311399d28f
mihawke <pratikgawari0000@gmail.com>
2024-11-07 12:24:38 +0530

    Files added

---
+++ TwoSum.js
@@ -0,0 +1,12 @@
+var twoSum = function(nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i+1; j < nums.length; j++) {
+            if(nums[i] + nums[j] == target){
+                return [i,j]
+            }
+        }
+    }
+};
+nums = [3,2,4], 
+target = 6
+console.log(twoSum(nums,target))
